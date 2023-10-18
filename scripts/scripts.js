51b2cc0701341ed87c9778fe7e7ba1b7533df5cf
Ana Liebert <analiebert19@gmail.com>
2023-10-18 17:23:34 -0400

    some site cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,26 +2,43 @@ window.onload=function(){
 
     let blueprint = document.querySelector(".blueprint")
     let generalAssembly = document.querySelector(".ga")
+    let freelance = document.querySelector(".freelance")
     let bpDesc = document.querySelector(".blueprint-desc")
     let gaDesc = document.querySelector(".ga-desc")
+    let freelanceDesc = document.querySelector(".freelance-desc")
    
-        
+    function hideBoth() {
+        freelance.classList.add("selected");
+        freelanceDesc.classList.remove("hide");
+        bpDesc.classList.add("hide");
+        gaDesc.classList.add("hide");
+        blueprint.classList.remove("selected");
+        generalAssembly.classList.remove("selected");
+        }
         function hideGA() {
         blueprint.classList.add("selected");
         gaDesc.classList.add("hide");
         bpDesc.classList.remove("hide");
         generalAssembly.classList.remove("selected");
+        freelanceDesc.classList.add("hide");
+        freelance.classList.remove("selected");
         }
         function hideBlueprint() {
         generalAssembly.classList.add("selected");
         bpDesc.classList.add("hide");
         gaDesc.classList.remove("hide");
         blueprint.classList.remove("selected");
+        freelanceDesc.classList.add("hide");
+        freelance.classList.remove("selected");
         }
 
 
         blueprint.addEventListener("click", hideGA);
         generalAssembly.addEventListener("click", hideBlueprint);
+        freelance.addEventListener("click", hideBoth);
+        
 
             
+
+
 }
